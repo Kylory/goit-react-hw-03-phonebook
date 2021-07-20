204c55b11d30c7cda69251f33d2955924d3ce93b
Kylory <72569852+Kylory@users.noreply.github.com>
2021-07-20 09:51:36 +0300

    added update to LS

--- src/App.js
+++ src/App.js
@@ -6,16 +6,43 @@ import shortid from 'shortid';
 
 class App extends Component {
   state = {
-    contacts: [],
+    contacts: [
+      // { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      // { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      // { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      // { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
     filter: '',
   };
+
+  // updateLocalStorage = () => {
+  //   const { contacts } = this.state;
+  //   localStorage.setItem('contacts', JSON.stringify(contacts));
+  // };
+
+  componentDidMount() {
+    console.log('Mount');
+    // const { contacts } = this.state;
+    const savedContacts = localStorage.getItem('contacts');
+    console.log(savedContacts);
+    this.setState({ savedContacts });
+    // const parsedSettings = JSON.parse(settingsFromLS);
+  }
+
+  componentDidUpdate() {
+    console.log('Update');
+    const { contacts } = this.state;
+    localStorage.setItem('contacts', JSON.stringify(contacts));
+  }
 
   formSubmitHandler = data => {
     const { name, number } = data;
     const { contacts } = this.state;
 
     this.setState({ name: name, number: number });
+    // console.log(this.state);
     contacts.push({ id: shortid.generate(), name: name, number: number });
+    // console.log(contacts);
   };
 
   changeFilter = e => {
